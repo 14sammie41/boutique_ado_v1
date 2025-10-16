6f8786f37138d3270ec4003cd5a426d47d02d7d3
14sammie41 <q85e7u2t@students.codeinstitute.net>
2025-10-16 18:11:34 +0100

    Edited safe procedures

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from bag.contexts import bag_contents
 
+import stripe
 
 def checkout(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    
+    if not settings.STRIPE_PUBLIC_KEY or not settings.STRIPE_SECRET_KEY:
+        messages.error(request, 'Stripe keys are not configured properly.')
+        return redirect(reverse('view_bag'))
+    
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
     
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    
+    try:
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+    except stripe.error.StripeError as e:
+        messages.error(request, f'Stripe error: {e}')
+        return redirect(reverse('view_bag'))
+    
+    print(intent)
+    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51SHl0bPW2LJfqxTFvgOCrDwzi0lCO0gMTIiumVFLMjmK6XCE978kF1KLV2OoElLa4g7bQd9anLjG9CXpcV3yd9s800k0x4lmHJ',
+        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
         'client_secret': 'test client secret',
     }
     
